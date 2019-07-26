d1651f64ca2326bf281cf13f4ebd2401b06e7046
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-07-26 21:16:11 +0200

    Init

---
+++ src/NPost.APIGateway/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Ntrada;
+
+namespace NPost.APIGateway
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+            => await WebHost.CreateDefaultBuilder(args)
+                .ConfigureServices(services => services
+                    .AddNtrada())
+                .Configure(app => app
+                    .UseNtrada())
+                .Build()
+                .RunAsync();
+    }
+}
